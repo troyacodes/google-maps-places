56c533c30ff2ac7bf768a65a82b7e0f7afa135ca
Michael <michaeltroya13@gmail.com>
2020-05-19 13:29:04 -0400

    inital map setup

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
 import React from 'react';
+
+import { GoogleMap, useLoadScript, Marker, InfoWindow } from '@react-google-maps/api';
+import { formatRelative } from 'date-fns';
+import '@reach/combobox/styles.css';
+
 import './App.css';
 
-function App() {
+const libraries = ['places'];
+const mapContainerStyle = {
+  width: '100vw',
+  height: '100vh',
+};
+const center = {
+  lat: 43.653225,
+  lng: -79.383186,
+};
+
+const App = () => {
+  const { isLoaded, loadError } = useLoadScript({
+    googleMapsApiKey: process.env.REACT_APP_MAPS_API_KEY,
+    libraries,
+  });
+
+  if (loadError) return 'Error loading maps';
+  if (!isLoaded) return ' Loading maps';
+
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>hi</h1>
-      </header>
+      <GoogleMap mapContainerStyle={mapContainerStyle} zoom={8} center={center}></GoogleMap>
     </div>
   );
-}
+};
 
 export default App;
